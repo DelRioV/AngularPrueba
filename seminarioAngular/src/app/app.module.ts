caf607c7dd92913f8fabf10a6eee9199596c5e78
pablo <pablosalvadordelriovergara.alumnado@sanjose.fundacionloyola.net>
2022-11-14 12:40:29 +0100

    Añadir bootstrap

--- seminarioAngular/src/app/app.module.ts
+++ seminarioAngular/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+
+import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
-  ],
+    FooterComponent,
+    HeaderComponent
+    ],
   imports: [
     BrowserModule
   ],
